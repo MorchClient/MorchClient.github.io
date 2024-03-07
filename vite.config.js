25f88a67df04ebdbb6db6b87cd3f1415d04763f6
WojtekGame <wojtek.game.private@gmail.com>
2024-03-07 16:18:28 +0100

    gh-pages update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/MorchClient/MorchClient.github.io/tree/gh-pages",
   plugins: [react()],
   build: {
     rollupOptions: {
